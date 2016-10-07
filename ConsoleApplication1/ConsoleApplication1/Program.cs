8b27cbe097c75977a4e4c9a92ae4769ed8bd8517
pavelsetrakov <pavelsetrakov@mailru>
2016-10-07 11:40:24 +0300

    Text1

--- ConsoleApplication1/ConsoleApplication1/Program.cs
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -10,6 +10,8 @@ namespace ConsoleApplication1
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Text1");
+            Console.ReadLine();
         }
     }
 }
